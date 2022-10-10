4314bec889059d576a576302028d56049da3a573
Keshav <keshav19249@iiitd.ac.in>
2022-10-10 14:11:37 +0530

    completed poll server client

---
+++ PollClientServer/client.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <chrono>
+#include <pthread.h>
+#define PORT 8080
+
+using namespace std;
+
+pthread_mutex_t mutexLock;
+vector<vector<long long int>> ansList;
+bool succesfullExec = true;
+bool unsuccesfullExec = false;
+
+void * createClient(void * args) {
+    vector<long long int> ans;
+    sleep(0.001);
+    int clientNum = *((int *)args);
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    inet_pton(AF_INET,"127.0.0.1",&serverAddress.sin_addr);
+    serverAddress.sin_port = htons(PORT);
+    int clientSocketFileDescriptor = socket(AF_INET,SOCK_STREAM,0);
+    if (clientSocketFileDescriptor < 0)
+    {
+        cout << "error creating socket" << endl;
+        return nullptr;
+    }
+
+    int connectionStatus = connect(clientSocketFileDescriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (connectionStatus < 0) {
+        cout << "error in connecting to server" << endl;
+        return nullptr;
+    }
+    else {
+        cout << "[Client:"<<clientNum<<"] connected successfully" << endl;
+    }
+
+    char buffer[1024];
+
+    for (int i = 1; i <= 20; i++) {
+        bzero(buffer, 1024);
+        const char *num = to_string(i).c_str();
+        int bytesSent = send(clientSocketFileDescriptor, num, strlen(num), 0);
+        read(clientSocketFileDescriptor, buffer, 1024);
+        pthread_mutex_lock(&mutexLock);
+        cout << "[Client:" << clientNum << "] number: " << i << " factorial: " << buffer << endl;
+        long long tmp = atoll(buffer);
+        ans.push_back(tmp);
+        pthread_mutex_unlock(&mutexLock);
+    }
+    pthread_mutex_lock(&mutexLock);
+    ansList.push_back(ans);
+    pthread_mutex_unlock(&mutexLock);
+    return nullptr;
+}
+
+int main() {
+    pthread_mutex_init(&mutexLock,nullptr);
+    int * clientCount = new int;
+    *clientCount = 1;
+    pthread_t threadArr[10];
+    for(int i = 1;i<=10;i++) {
+        pthread_create(&threadArr[i-1],NULL,createClient,clientCount);
+        (*clientCount)++;
+        char * tmp;
+    }
+    for(int i = 0;i<10;i++) {
+        pthread_join(threadArr[i],NULL);
+    }
+    cout<<"over here"<<endl;
+    cout<<endl<<endl<<endl;
+    for(int i = 0;i<ansList.size();i++) {
+        for(long long int val : ansList[i]) {
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ PollClientServer/server.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <fstream>
+#include <sys/file.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <sys/poll.h>
+
+#define PORT 8080
+using namespace std;
+
+struct SocketDescriptor {
+    int socketFileDescriptor;
+    string clientIP;
+    string clientPort;
+};
+
+long long int getFactorialOfNumber(int num) {
+    if(num == 0)
+        return 1;
+    long long int factorial = 1;
+    for(int i = 1;i<=num;i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+void printFileDescArray(struct pollfd * fds,int totalCount) {
+    for(int i = 0;i<totalCount;i++) {
+        cout<<fds[i].fd<<"   "<<fds[i].events<<"   "<<fds[i].revents<<endl;
+    }
+}
+
+int main() {
+    struct sockaddr_in serverAddress;
+    socklen_t clientLength;
+    int cntClientCount = 0;
+    int maxClients = 0;
+    unordered_map <int,struct SocketDescriptor *> clientDescMap;
+    fstream outFile;
+    struct pollfd fileDescriptors[20];
+    // int cntFileDescCount = 0;
+    
+    bzero((char *)&serverAddress,sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+
+//  Socket System Call
+    int serverSocketFileDescriptor = socket(AF_INET,SOCK_STREAM,0);
+    if(serverSocketFileDescriptor < 0) {
+        cout<<"Error in creating main socket"<<endl;
+        return -1;
+    }
+
+//  Bind System Call
+    int socketBind = bind(serverSocketFileDescriptor,(struct sockaddr *)&serverAddress,sizeof(serverAddress));
+    if(socketBind < 0) {
+        cout<<"Error in binding to address"<<endl;
+        return -1;
+    }
+
+//  Listen System Call
+    listen(serverSocketFileDescriptor,20);
+
+    for(int i = 0;i<20;i++) {
+        fileDescriptors[i].fd = 0;
+        fileDescriptors[i].events = POLL_IN;
+    }
+
+    fileDescriptors[0].fd = serverSocketFileDescriptor;
+    fileDescriptors[0].events = POLL_IN;
+    cntClientCount++;
+    maxClients++;
+    while(true) {
+        if(maxClients == 11 && cntClientCount == 1)
+            break;
+        int cntActiveFileDesc = poll(fileDescriptors,cntClientCount,0);
+        if(cntActiveFileDesc < 0) {
+            cout<<"Error in polling"<<endl;
+            return -1;
+        }
+        int activeFileDesc = cntClientCount;
+        for(int i = 0;i<activeFileDesc;) {
+            if(i == 0 && (fileDescriptors[i].revents & POLL_IN)) {
+                struct sockaddr_in clientAddress;
+                clientLength = sizeof(clientAddress);
+                struct SocketDescriptor * newSocketDescriptor;
+                newSocketDescriptor = new SocketDescriptor();
+                newSocketDescriptor->socketFileDescriptor = accept(serverSocketFileDescriptor,(struct sockaddr *)&clientAddress,&clientLength);
+                if(newSocketDescriptor->socketFileDescriptor < 0) {
+                    cout<<"error in creating new client"<<endl;
+                    return -1;
+                }
+                newSocketDescriptor->clientIP = to_string(clientAddress.sin_addr.s_addr);
+                newSocketDescriptor->clientPort = to_string(clientAddress.sin_port);
+                clientDescMap[newSocketDescriptor->socketFileDescriptor] = newSocketDescriptor;
+                fileDescriptors[cntClientCount].fd = newSocketDescriptor->socketFileDescriptor;
+                fileDescriptors[cntClientCount].events = POLL_IN;
+                cntClientCount++;
+                maxClients++;
+
+                if(maxClients == 2) {
+                    outFile.open("clientServerFile.txt",ios::out);
+                }
+            }
+
+            if(i != 0 && (fileDescriptors[i].revents & POLL_IN)) {
+                char buffer[1024];
+                memset(buffer,0,1024);
+                int bytesRead = read(fileDescriptors[i].fd,buffer,1024);
+                if(bytesRead <= 0) {
+                    cout<<"closing socket"<<endl;
+                    close(fileDescriptors[i].fd);
+                    for(int j = i+1;j<cntClientCount;j++) {
+                        fileDescriptors[j-1].fd = fileDescriptors[j].fd;
+                        fileDescriptors[j-1].events = fileDescriptors[j].events;
+                        fileDescriptors[j-1].revents = fileDescriptors[j].revents;
+                    }
+                    cntClientCount--;
+                    activeFileDesc--;
+                    continue;
+                }
+                int num = atoi(buffer);
+                long long int factorial = getFactorialOfNumber(num);
+                string factString = to_string(factorial);
+                const char *factCharArr = factString.c_str();
+                cout<<"[ClientIP: "<<clientDescMap[fileDescriptors[i].fd]->clientIP<<"   ClientPort: "<<clientDescMap[fileDescriptors[i].fd]->clientPort<<"]   Number: "<<num<<"   Factorial: "<<factorial<<endl;
+                string finalFileWriteString =  "[ClientIP: "+clientDescMap[fileDescriptors[i].fd]->clientIP+"   ClientPort: "+clientDescMap[fileDescriptors[i].fd]->clientPort+"]   Number: "+to_string(num)+"   Factorial: "+to_string(factorial)+"\n";
+                outFile<<finalFileWriteString;
+                send(fileDescriptors[i].fd,factCharArr,strlen(factCharArr),0);
+            }
+            i++;
+        }
+    }
+    outFile.close();
+    shutdown(serverSocketFileDescriptor,SHUT_RDWR);
+    return 0;
+}
